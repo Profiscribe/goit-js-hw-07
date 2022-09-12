1ad928d408a94a903a5b99bb414da4bffa02d8ae
Оксана Качмар <108275501+Profiscribe@users.noreply.github.com>
2022-09-12 19:47:25 +0300

    HW 08 js finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
+const galleryItemsList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markUp = onCreatePicture(galleryItems);
+{/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> */}
+galleryItemsList.insertAdjacentHTML('beforeend', markUp);
+function onCreatePicture(items) {
+    return items.map(item => {
+        return `
+       <div class="gallery__item">
+  <a class="gallery__link" href="${item.preview}"
+  data-lightbox = "lbox">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`}).join('')
+}
+
+galleryItemsList.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+		return
+	}
+    const selectedImage = e.target.getAttribute('data-source')
+    const instance = basicLightbox.create(`<img src="${selectedImage}" width="800" height="600">`)
+    instance.show()
+        galleryItemsList.addEventListener('keydown', e => {
+		if (e.key === 'Escape') {
+			instance.close()
+		}
+	})
+})
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
+const galleryItemsList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markUp = onCreatePicture(galleryItems);
+
+
+galleryItemsList.insertAdjacentHTML('beforeend', markUp);
+function onCreatePicture(items) {
+	return galleryItems.map(({ preview, original, description }) => {
+        return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title ="${description}"/>
+</a>`}).join('')
+}
+
+	function onImageClick(e) {
+		e.preventDefault();
+		if (e.target.nodeName !== 'IMG') {
+			return
+		}
+	}
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
